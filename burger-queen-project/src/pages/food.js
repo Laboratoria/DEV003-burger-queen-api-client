a3266037484fb5dc25e3f0a6ef61671016c4e0ec
RossannaAriza <rossanna.ariza@gmail.com>
2023-04-25 11:28:02 -0500

    add components

--- burger-queen-project/src/pages/food.js
+++ burger-queen-project/src/pages/food.js
@@ -1,15 +1,14 @@
 import { HeaderMenu } from "../components/menu/headerMenu";
 import { NavigationMenu } from "../components/menu/navigationMenu";
-import { BoXBurgerDance } from "../components/menu/boxBurgerDance";
 import { BoXOrder } from "../components/menu/boxOrder";
+import { BoxCategoriesMenuFood } from "../components/menu/boxCategoriesMenuFood";
 import { BoxProcess } from "../components/menu/boxProcess";
 
-
-export default function MenuPage() {
+export default function MenuFoodPage() {
     return (<div>
         <HeaderMenu />
         <NavigationMenu />
-        <BoXBurgerDance />
+        <BoxCategoriesMenuFood />
         <BoXOrder />
         <BoxProcess />
     </div>
