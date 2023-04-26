6d5605e8247a0fd9ea152ca38abd1ed4a6ba173a
Cheilanthe <dianaehg1003@gmail.com>
2023-04-26 00:05:28 -0600

    actualiza estilos de btn

--- burger-queen-project/src/components/menu/navigationMenu.js
+++ burger-queen-project/src/components/menu/navigationMenu.js
@@ -1,11 +1,14 @@
+import { Style_Script } from 'next/font/google';
 import { useRouter } from 'next/router';
+import styles from "../../styles/boxBurgerDance.module.css"
 
 export function NavigationMenu () {
   const router = useRouter();
     return (
-      <div className="NavigationMenu">
-        <nav className="NavigationMenu-nav">
-               <button className="NavigationMenu-nav-btnBreakfast"onClick={()=>router.push('/desayuno')}>Desayuno</button> <button className="Navigation-nav-btnFood" onClick={()=>router.push('/food')}>Almuerzo</button> 
+      <div className={styles.NavigationMenu}>
+        <nav className={styles.NavigationMenunav}>
+               <button className={styles.NavigationbtnBreakfast} onClick={()=>router.push('/desayuno')}>Desayuno</button> 
+               <button className={styles.NavigationbtnLunch} onClick={()=>router.push('/food')}>Almuerzo</button> 
             </nav>
             </div>
     )
