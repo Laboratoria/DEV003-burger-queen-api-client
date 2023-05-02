59f04c86601a992e43e39d526d091165925ad51b
Cheilanthe <dianaehg1003@gmail.com>
2023-05-02 08:28:07 -0600

    renderizado y el contador de los btns

---
+++ burger-queen-project/src/components/menu/counterButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useState } from "react";
+import style from "../../styles/all-food.module.css";
+
+export const CounterButton = () => {
+let [count, setCount] = useState(0);
+
+const counterAdd = () => { setCount (c => c + 1)};
+const counterLess = () => { count <= 0 ? count = 0: setCount (c => c - 1) };
+
+return (
+    <>
+     <button className={style.btnAdd} onClick= {() => counterLess()}>-</button>
+     <div className={style.counter}><p>{count}</p></div>
+     <button className={style.btnAdd} onClick= {() => counterAdd()}>+</button>
+    </>
+   
+)}
