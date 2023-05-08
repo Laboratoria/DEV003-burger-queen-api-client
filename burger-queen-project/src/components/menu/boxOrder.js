71f9735a67b3ab09ee158a998410b327ebe9c2f6
RossannaAriza <rossanna.ariza@gmail.com>
2023-05-08 13:54:47 -0500

    print the products on the page

--- burger-queen-project/src/components/menu/boxOrder.js
+++ burger-queen-project/src/components/menu/boxOrder.js
@@ -1,26 +1,38 @@
 import styles from "../../styles/boxOrder.module.css";
-import { useState } from "react";
+import { ContextOrder } from "./contextOrder";
+import { useContext } from "react";
 
-export function BoxOrder(props) {
-    // const [order, setOrder] = useState([{
-    //     products: [],
-    //     total: 0,
-    //     customer: { name: '' }
-    // }]);
-    // const handleOrder = (e) => {
-    //     // const { nameProduct, value } = e.target.value;
-    //     // setOrder((preOrder) => ({
-    //     //     ...preOrder,
-    //     //     products: [preOrder.products],
-    //     // }))
-    //     setValue(e.target.value);
-    // };
+export function BoxOrder() {
+    const [order] = useContext(ContextOrder);
+    console.log(order)
     return (
         <div className={styles.burgerBox}>
             <label className={styles.customer}>Cliente: <input type="text" className={styles.customerName}></input></label>
-            <div>
-            <p>{props.counter}</p>
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Producto</th>
+                        <th>Cantidad</th>
+                        <th>Costo</th>
+                    </tr>
+                </thead>
+                <tbody >
+                    {order.map((item, index) => {
+                        console.log(item[index].name)
+                        return (
+                            <tr key={index}>
+                                <td>{item[index].name}</td>
+                                <td>{item[index].counter}</td>
+                                <td>$ {item[index].price}</td>
+                            </tr>
+                        )
+                    })}
+                    <tr>
+                        <td>Total: $</td>
+                    </tr>
+                </tbody>
+            </table>
+
         </div>
 
     )
