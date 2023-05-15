35f3376b7597ee42ade5a2367ad375d4cb10c19d
Luisa <lorenitaa889@gmail.com>
2023-05-15 10:35:49 -0500

    Mock + test product

--- __tests__/waiter.test.js
+++ __tests__/waiter.test.js
@@ -1,9 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Waiter from "@/pages/waiter";
 import '@testing-library/jest-dom';
  
+jest.mock('axios', () => ({
+    get: () => {
+      return  Promise.resolve({
+        data: [    
+          {
+            "id": 1,
+            "name": "Sandwich de jamón y queso",
+            "price": 1000,
+            "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/sandwich.png",
+            "type": "Desayuno",
+            "dateEntry": "2022-03-05 15:14:10"   
+          },
+          {
+            "id": 2,
+            "name": "Café americano",
+            "price": 500,
+            "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/coffee.png",
+            "type": "Desayuno",                    
+            "dateEntry": "2022-03-05 15:14:10"
+          }
+        ]
+      })    
+    }
+})) 
+
 describe('Home', () => {
-  it('renders a header with title', () => {
+  it('renders a header with title', async () => {
     render(<Waiter />);
  
     const heading = screen.getByRole('heading', {
@@ -11,5 +36,6 @@ describe('Home', () => {
     });
  
     expect(heading).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText('Café americano')).toBeTruthy())
   });
 });
--- src/pages/components_waiter/productsContext.js
+++ src/pages/components_waiter/productsContext.js
@@ -2,15 +2,13 @@ import {useState, useEffect, createContext} from 'react'
 import axios from 'axios'
 
 export const ProductsContext = createContext()
-export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBydWViYWx1aXNhQGdtYWlsLmNvbSIsImlhdCI6MTY4Mzc0MzM5NCwiZXhwIjoxNjgzNzQ2OTk0LCJzdWIiOiIzIn0.IAZ1B4xz8UbxFDNbjuA0UTMdEEVmYTOdwMEtPOLNME8"
+export const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBydWViYWx1aXNhQGdtYWlsLmNvbSIsImlhdCI6MTY4Mzg0MDYyNCwiZXhwIjoxNjgzODQ0MjI0LCJzdWIiOiIzIn0.HK0xum8F8ZVvvPU7TyH6gLnMB9JsMcw_eYN4kkBPREw'
 export const ProductContextProvider = ({children}) => {
     const [products, setProducts] = useState([])
 
     useEffect(() => {
         
-        axios({
-            method : 'GET',
-            url: 'http://localhost:8080/products',
+        axios.get('http://localhost:8080/products', {
             headers: {
                 Authorization: `Bearer ${token}`
             }
